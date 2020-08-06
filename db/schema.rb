fea76a11db4379bc92a03baebb07596047dd816f
NizarMrad <nizar.mrad.pro@gmail.com>
2020-08-06 23:50:38 +0200

    version 3

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 2020_08_06_151823) do
   create_table "gossips", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_gossips_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
